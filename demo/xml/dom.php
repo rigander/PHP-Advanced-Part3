6ff0fe66cc6699cbad125cefa6a03849b1ca1d0c
Andrey Komov <andrewkomov@gmail.com>
2023-03-07 23:19:29 +0200

    PHP-XML-DOM

--- demo/xml/dom.php
+++ demo/xml/dom.php
@@ -1,23 +1,34 @@
 <?php 
-  header( "Content-Type: text/html;charset=utf-8"); 
+  header( "Content-Type: text/html;charset=utf-8");
+  //todo Создадим экземпляр класса.
+  $dom = new DOMDocument();
+  //todo Загрузим в него документ.
+  $dom->load("catalog.xml");
+  //todo Получим корневой каталог.
+  $root = $dom->documentElement;
+//todo Пропустили коллекцию узлов через foreach и вывели содержимое каждого узла.
+foreach($root->childNodes as $book){
+    echo $book->textContent. "<br>";
+}
+
 ?>
 <html>
 
 <head>
-  <title>Каталог</title>
+  <title>Catalog-DOM</title>
 </head>
 
-<body>
-  <h1>Каталог книг</h1>
+<body style="background-color: #3c3f41; color: #bd6f31">
+  <h1>Books catalog</h1>
   <table border="1" width="100%">
     <tr>
-      <th>Автор</th>
-      <th>Название</th>
-      <th>Год издания</th>
-      <th>Цена, руб</th>
+      <th>Author</th>
+      <th>Name</th>
+      <th>Year published</th>
+      <th>Price, usd</th>
     </tr>
     <?php 
-      //Парсинг 
+      //Парсинг
     ?>
   </table>
 </body>
