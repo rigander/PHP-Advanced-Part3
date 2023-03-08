eebb3423b41e4c8dc94acb756d85781d62d733ca
Andrey Komov <andrewkomov@gmail.com>
2023-03-09 00:08:15 +0200

    laba3.2

--- demo/xml/simplexml.php
+++ demo/xml/simplexml.php
@@ -1,23 +1,36 @@
 <?php 
-  header( "Content-Type: text/html;charset=utf-8"); 
+  header( "Content-Type: text/html;charset=utf-8");
+  //todo Загружаем файл. Фактически получаю в объект корневой элемент
+  // со всем его содержимым.
+  $sxml = simplexml_load_file("catalog.xml");
+
+
 ?>
 <html>
 
 <head>
-  <title>Каталог</title>
+  <title>Catalog</title>
 </head>
 
-<body>
-  <h1>Каталог книг</h1>
+<body style="background-color: #3c3f41; color: #bd6f31;">
+  <h1>Book Catalog</h1>
   <table border="1" width="100%">
     <tr>
-      <th>Автор</th>
-      <th>Название</th>
-      <th>Год издания</th>
-      <th>Цена, руб</th>
+      <th>Author</th>
+      <th>Name</th>
+      <th>Year published</th>
+      <th>Price, usd</th>
     </tr>
     <?php 
-      //Парсинг 
+      //Парсинг
+    foreach ($sxml->book as $book){
+        echo "<tr>";
+        echo "<td>{$book->author}</td>";
+        echo "<td>{$book->title}</td>";
+        echo "<td>{$book->pubyear}</td>";
+        echo "<td>{$book->price}</td>";
+        echo "</tr>";
+    }
     ?>
   </table>
 </body>
