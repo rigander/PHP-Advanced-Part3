599b68e7a522e704fdc2193b1c753aa095db68c0
Andrey Komov <andrewkomov@gmail.com>
2023-03-16 04:54:10 +0200

    CAPTCHA laba6-Session-Error

--- gd/noise-picture.php
+++ gd/noise-picture.php
@@ -1,1 +1,52 @@
 <?php
+$img = imagecreatefromjpeg("images/noise.jpg");
+
+$colors = [
+  0 => $red = imagecolorallocate($img, 255, 0,0),
+  1 => $blue = imagecolorallocate($img, 0, 0,255),
+  2 => $black = imagecolorallocate($img, 0, 0,0),
+  3 => $green =  imagecolorallocate($img, 0, 255,0),
+  4 => $violet =  imagecolorallocate($img, 127, 0,255),
+  5 => $purple =  imagecolorallocate($img, 128, 0,128)
+];
+
+$fonts = [
+    0 => $bell1 = "fonts/bellb.ttf",
+    1 => $georgia1 = "fonts/georgia.ttf",
+    2 => $georgia2 = "fonts/georgia.ttf",
+    3 => $bell2 = "fonts/bellb.ttf",
+    4 => $georgia3 = "fonts/georgia.ttf",
+    5 => $bell3 = "fonts/bellb.ttf",
+];
+
+$red = imagecolorallocate($img, 255, 0,0);
+imageAntiAlias($img, true);
+
+$string = bin2hex(random_bytes(3));
+$count = strlen($string);
+
+$angle = 0;
+$size = 20;
+for ($l = 0 & $x = 20;
+     $l <= $count;
+     $l++ & $x +=35
+){
+    $angle +=15;
+    $randomSize = rand(15, 30);
+    $randomAngle = rand(15, 90);
+    $randomColor = rand(0, 5);
+    $rc = $randomColor;
+    $randomFont = rand(0, 5);
+    imagettftext($img, $randomSize, $randomAngle, $x, 30, $colors[$rc],
+        $fonts[$randomFont], $string{$l});
+}
+
+header("Content-type: image/jpg");
+imagejpeg($img, null, 50);
+
+session_start();
+$_SESSION["captchaString"] = $string;
+
+
+
+
--- gd/registration.php
+++ gd/registration.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 ?>
 <!DOCTYPE HTML>
@@ -6,23 +6,39 @@
 
 <head>
   <meta charset="utf-8" />
-  <title>Регистрация</title>
+  <title>Registration</title>
+  <link rel="stylesheet" href="styles-gd.css">
 </head>
 
 <body>
-  <h1>Регистрация</h1>
+  <h2>Captcha</h2>
+  <br>
   <form action="" method="post">
     <div>
       <img src="noise-picture.php">
     </div>
+    <br>
     <div>
-      <label>Введите строку</label>
-      <input type="text" name="answer" size="6">
+      <label>Enter String</label>
+      <input class="input-field" type="text" name="answer" size="6">
     </div>
-    <input type="submit" value="Подтвердить">
+    <br>
+    <input class="submit" type="submit" value="Send">
   </form>
-  <?php 
-  
+  <br>
+  <?php
+  var_dump($_POST);
+  var_dump($_SESSION);
+
+  if(!$_POST["answer"]){
+     echo "Please fill up captcha";
+  }
+  elseif ($_POST["answer"] === $_SESSION["captchaString"]){
+      echo "All if fine";
+  }else{
+      echo "Mistake. Please try again.";
+  }
+
   ?>
 </body>
 
